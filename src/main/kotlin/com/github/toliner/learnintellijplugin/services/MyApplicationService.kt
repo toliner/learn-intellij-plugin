1185e639a86b6dabeb8cdfd596a53c10cb917ec3
GitHub Action <action@github.com>
2022-06-21 06:00:23 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/toliner/learnintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.toliner.learnintellijplugin.services
+
+import com.github.toliner.learnintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
